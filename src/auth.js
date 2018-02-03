156cc3dabbd9dd61c8c61eab77556f3adec98913
Mikhail Ignatiev <st6legion@gmail.com>
2018-02-03 15:20:55 +0300

    first working prototype

---
+++ src/auth.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+function Auth(page) {
+    const VK_COOKIES_FILE = '../vk_cookies.json';
+    const AWAIT_LOGIN_TIMEOUT = 60000 * 5;//5 min
+
+    this.page = page;
+
+    this.login = async () => {
+        if (!fs.existsSync(VK_COOKIES_FILE)) {
+            await page.goto('https://vk.com');
+            await page.waitFor(1000);
+            await page.waitForSelector('#l_pr', {timeout: AWAIT_LOGIN_TIMEOUT});
+            this.saveCookies(await page.cookies());
+        }
+        else{
+            await this.loadCookies();
+            await page.waitFor(1000);
+            await page.goto('https://vk.com');
+            await page.waitFor(1000);
+        }
+    };
+
+    this.saveCookies = cookies => {
+        fs.writeFile(VK_COOKIES_FILE, JSON.stringify(cookies), err => {
+            if (err) throw err;
+        });
+    };
+
+    this.loadCookies = async () => {
+        await fs.readFile(VK_COOKIES_FILE, async (err, cookies) => {
+            if (err) {
+                console.log(err);
+            } else {
+                    await page.setCookie(...JSON.parse(cookies));
+            }
+        });
+    };
+
+
+}
+
+module.exports = Auth;
---
+++ src/index.js
@@ -0,0 +1,129 @@
+const Auth = require('./auth');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const HEADLESS = false;
+const VK_URLS = ['https://vk.com/id243451378', 'https://vk.com/id317908041', 'https://vk.com/lastcallfilm'];
+// const VK_URLS = ['https://vk.com/id243451378', 'https://vk.com/id317908041'];
+// const VK_URLS = ['https://vk.com/lastcallfilm'];
+
+let browser;
+let results = {};
+let page;
+
+(async () => {
+    browser = await puppeteer.launch({headless: HEADLESS, dumpio: true});
+    page = await browser.newPage();
+    await page.goto('https://vk.com');
+    await page.waitFor(1000);
+    await new Auth(page).login();
+
+    await scrapVk();
+    await browser.close();
+})();
+
+const scrapVk = async () => {
+    for (let i = 0; i < VK_URLS.length; i++) {
+        let url = VK_URLS[i];
+        let urlResults = {};
+        await page.goto(url);
+        await page.waitFor(1000);
+        await page.evaluate(() => document.styleSheets[0].insertRule(".fans_fan_img { display: none; }", 1));
+
+        if (await isPersonalPage()) {
+            urlResults = await scrapVkFriends(urlResults);
+            urlResults = await scrapVkSubscribers(urlResults);
+        }
+        else {
+            urlResults = await scrapVkParticipants(urlResults);
+        }
+        results[url] = urlResults;
+    }
+
+    await fs.writeFile('../result.json', JSON.stringify(results), err => {
+        if (err) throw err;
+        console.log("results file was saved!");
+    });
+};
+
+const scrapVkFriends = async urlResults => {
+    await closeFriendsOrSubscribersBox();
+    await clickFriendsOrSubscribersButton('friends');
+    await scrollContainerToBottom('#box_layer_wrap');
+    return await scrapFriendsOrSubscribersData('friend', urlResults, '.fans_fan_lnk');
+};
+
+const scrapVkSubscribers = async urlResults => {
+    await closeFriendsOrSubscribersBox();
+    await clickFriendsOrSubscribersButton('#');
+    await scrollContainerToBottom('#box_layer_wrap');
+    return await scrapFriendsOrSubscribersData('subscriber', urlResults);
+};
+
+const scrapFriendsOrSubscribersData = async (key, urlResults, selector) => {
+    return await page.evaluate((key, urlResults, selector) => {
+        [...document.querySelectorAll(selector)]
+            .forEach(a => {
+                let userId = a.href.split('/').pop();
+                if (!urlResults[userId]) {
+                    urlResults[userId] = {};
+                }
+                urlResults[userId].name = a.text;
+                urlResults[userId][key] = true;
+            });
+        return urlResults;
+    }, key, urlResults, selector);
+};
+
+const clickFriendsOrSubscribersButton = async buttonHrefFragment => {
+    await page.evaluate(buttonHrefFragment => {
+        [...document.querySelectorAll('.page_counter')]
+            .find(a => a.href.includes(buttonHrefFragment))
+            .click();
+    }, buttonHrefFragment);
+    await page.waitFor(400);
+};
+
+const closeFriendsOrSubscribersBox = async () => await page.evaluate(() => {
+    let closeButton = document.querySelector('.box_x_button');
+    if (closeButton) {
+        closeButton.click();
+    }
+});
+
+const scrollContainerToBottom = async (selector) => {
+    let lastScroll = 0;
+    while (true) {
+        await page.keyboard.press('PageDown');
+        await page.waitFor(400);
+        let currentScroll = await page.evaluate(selector => document.querySelector(selector).scrollTop, selector);
+        if (lastScroll === currentScroll) {
+            break;
+        }
+        lastScroll = currentScroll;
+    }
+};
+
+const scrapVkParticipants = async urlResults => {
+    await closeFriendsOrSubscribersBox();
+
+    await page.evaluate(() => document.querySelector('#group_followers > a').click());
+    await page.waitFor(500);
+    let searchPageUrl = await page.evaluate(() => document.querySelector('a.ui_box_search').href);
+    await page.goto(searchPageUrl);
+    await page.waitFor(2000);
+    await page.evaluate(() => document.styleSheets[0].insertRule(".search_item_img { display: none; }", 1));
+
+    await page.click('#region_filter');
+    await page.keyboard.type('Россия', {delay: 100});
+    await page.keyboard.press('Enter');
+
+    await scrollContainerToBottom('html');
+    urlResults = await scrapFriendsOrSubscribersData('participant', urlResults, '.labeled.name > a');
+
+    return urlResults;
+};
+
+const isPersonalPage = async () => {
+    return 0 !== await page.evaluate(() => document.querySelectorAll('.page_counter').length);
+};
